3f28f3fa1d45ab17543ac157f1eac1158c9a8917
kyllo34 <kyllo34@gmail.com>
2019-12-10 16:24:16 -0800

    replaced object literals with a single constructor function

--- js/app.js
+++ js/app.js
@@ -1,88 +1,73 @@
 'use strict';
-var hrs = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+// hours of operation
+var hrs = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var total = 0;
+// Creates city objects
+var seattle = new City('Seattle', 3, 18, 4.15);
+var tokyo = new City('Tokyo', 3, 24, 1.2);
+var dubai = new City('Dubai', 11, 38, 3.7);
+var paris = new City('Paris', 20, 38, 2.3);
+var lima = new City('Lima', 2, 16, 4.6);
+var stores = [seattle, tokyo, dubai, paris, lima];
 
-var seattle = {
-  name: 'Seattle',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookieSale: 6.3,
-  // function that chooses a random number between the min and max customer
-  custPerhour: function() {
-    return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-  },
-  // function that outputs am average calulculated cookies sold every hour
-  cookiesPerHour: function() {
-    return this.custPerhour() * this.avgCookieSale;
-  },
+// Object constructor for cityies that take in arguments
+function City(name, minCustomer, maxCustomer, avgCookieSale) {
+  this.name = name;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSale = avgCookieSale;
+}
+// method that chooses a random number between the min and max customer
+City.prototype.getRandomCookieSale = function() {
+  return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
 };
-var tokyo = {
-  name: 'Tokyo',
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookieSale: 1.2,
-  custPerhour: function() {
-    return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-  },
-  cookiesPerHour: function() {
-    return this.custPerhour() * this.avgCookieSale;
-  },
+// method that outputs am average calulculated cookies sold every hour
+City.prototype.cookiesSoldHr = function() {
+  return Math.ceil(this.getRandomCookieSale() * this.avgCookieSale);
 };
-var dubai = {
-  name: 'Dubai',
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookieSale: 3.7,
-  custPerhour: function() {
-    return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-  },
-  cookiesPerHour: function() {
-    return this.custPerhour() * this.avgCookieSale;
-  },
-};
-var paris = {
-  name: 'Paris',
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookieSale: 2.3,
-  custPerhour: function() {
-    return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-  },
-  cookiesPerHour: function() {
-    return this.custPerhour() * this.avgCookieSale;
-  },
-};
-var lima = {
-  name: 'Lima',
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookieSale: 4.6,
-  custPerhour: function() {
-    return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-  },
-  cookiesPerHour: function() {
-    return this.custPerhour() * this.avgCookieSale;
-  },
-};
-var cities = [seattle, tokyo, dubai, paris, lima];
-var cityHolder = document.getElementById('city-holder');
 
-for(var j = 0; j < cities.length; j++) {
-  // assigns variable to <p> tag
-  var newP = document.createElement('p');
-  // insert city name in <p> content
-  newP.textContent = cities[j].name;
-  // append <p> with content to cityHolder
-  cityHolder.appendChild(newP);
-  // create loop that goes through each open hour of day
-  for(var i = 6; i < 19; i++) {
-  // time + cookies sold in that hour
-  // create variable equal to each new line
-    var newLi = document.createElement('li');
-    // each line contecnt equal to hour of day and cookies sold in that hour
-    newLi.textContent = `${hrs[i - 6]}: ${Math.ceil(cities[j].cookiesPerHour())}`;
-    // append that content to city holder
-    cityHolder.appendChild(newLi);
-  }
+var tableHolder = document.getElementById('table-holder');
+
+for(var i = 0; i < stores.length; i++) {
+  var cityRow = document.createElement('tr');
+  var cityData = document.createElement('td');
+  cityData.textContent = stores[i].name;
+  cityRow.appendChild(cityData);
+  tableHolder.appendChild(cityRow);
+  // for(var j = 0; j < hrs.length; j++) {
+  //   cityData.textContent = stores[i].cookiesSoldHr();
+  //   cityRow.appendChild(cityData);
+  //   tableHolder.appendChild(cityRow);
+  // }
+
 }
 
+// var cityCell = document.createElement('td');
+// cityCell.textContent = stores[i].name;
+// tableHolder.appendChild(cityCell);
+
+// var cityHolder = document.getElementById('city-holder');
+
+// for(var j = 0; j < cities.length; j++) {
+//   var total = 0;
+//   // assigns variable to <p> tag
+//   var newP = document.createElement('p');
+//   // insert city name in <p> content
+//   newP.textContent = cities[j].name;
+//   // append <p> with content to cityHolder
+//   cityHolder.appendChild(newP);
+//   // create loop that goes through each open hour of day
+//   for(var i = 6; i < 19; i++) {
+//   // time + cookies sold in that hour
+//   // create variable equal to each new line
+//     var newLi = document.createElement('li');
+//     // each line contecnt equal to hour of day and cookies sold in that hour
+//     newLi.textContent = `${hrs[i - 6]}: ${Math.ceil(cities[j].cookiesPerHour())}`;
+//     // append that content to city holder
+//     cityHolder.appendChild(newLi);
+//     total += Math.ceil(cities[j].cookiesPerHour());
+//   }
+//   newLi.textContent = `Total: ${total}`;
+//   cityHolder.appendChild(newLi);
+// }
 
